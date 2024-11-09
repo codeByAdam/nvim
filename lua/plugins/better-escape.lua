435fa921b66ed0063661a7e3e60e523e468a6c75
Adam Ordway <adamordway@Mac.local>
2024-11-09 00:12:04 -0500

    updated better escape. Removed flutter

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,9 +1,32 @@
+key_map = {
+  --  first_key[s]
+  j = {
+    --  second_key[s]
+    k = "<Esc>",
+  },
+  k = {
+    --  second_key[s]
+    j = "<Esc>",
+  },
+}
 return {
   "max397574/better-escape.nvim",
   config = function()
     require("better_escape").setup({
-      mapping = { "jk", "kj", "jj" },
-      timeout = 100,
+      mappings = {
+        i = key_map,
+        c = key_map,
+        t = {
+          j = {
+            k = "<C-\\><C-n>",
+          },
+          k = {
+            j = "<C-\\><C-n>",
+          },
+        },
+        v = key_map,
+        s = key_map,
+      },
     })
   end,
 }
--- lua/plugins/flutter-tools.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "akinsho/flutter-tools.nvim",
-  config = function()
-    require("flutter-tools").setup()
-  end,
-}
